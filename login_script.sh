b4ac4b68b11faad49d93725aa4c86751efb8f0f7
user <user@NUC32>
2019-10-01 08:23:18 -0300

    Added script file as a reference

---
+++ login_script.sh
@@ -0,0 +1,18 @@
+# author: Henrique Pereira Rosa
+# data: 19/09/2019
+# File to clean network connections and
+#  advice the user.
+
+#!bin/bash
+
+
+#sudo -i java -jar /home/user/dev/projects/linux-scripts-java/output/LinuxScriptsJava.jar
+#java -jar /etc/profile.d/jar/LinuxScriptsJava.jar 
+#sudo rm -rf /etc/NetworkManager/system-connections/*
+
+
+#zenity --info \ DIDN'T WORK
+# --text="Logging IN." \
+# --title="LOGING  MESSAGE" \
+# --width=800 --height=800
+
